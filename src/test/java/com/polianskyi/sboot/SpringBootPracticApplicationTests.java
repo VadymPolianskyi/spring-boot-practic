4b41ba90798cb73dceb60ccbdcf53c16fd46828b
vadym <vadym.polyanski@gmail.com>
2018-01-08 22:57:13 +0200

    Initial commit

---
+++ src/test/java/com/polianskyi/sboot/SpringBootPracticApplicationTests.java
@@ -0,0 +1,16 @@
+package com.polianskyi.sboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootPracticApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
