17aab1e52ae7a01d849b7caf1be8be615df296ed
VadymPolyanski <byrfcfnjh09>
2018-01-09 19:37:47 +0200

    best solution for main class

--- src/main/java/com/polianskyi/sboot/SpringBootPracticApplication.java
+++ src/main/java/com/polianskyi/sboot/SpringBootPracticApplication.java
@@ -2,19 +2,16 @@ package com.polianskyi.sboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@RestController
+@Configuration
 @EnableAutoConfiguration
+@ComponentScan
 public class SpringBootPracticApplication {
-
-	@RequestMapping("/")
-	String home() {
-		return "Home!";
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootPracticApplication.class, args);
 	}
+
 }
